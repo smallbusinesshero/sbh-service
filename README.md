65268ca48bdbca8b03945fcfde500fc9d56d80cc
schugnic <nicolai.schug@diconium.com>
2020-03-20 20:22:43 +0100

    Init SpringBoot App

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dds-wvv-backend
+
+## Install openjdk8 and maven 
+## Please use Lombok and dont waste any time to write dtos
---
+++ src/main/java/com/diconiumwvv/storesservice/StoresserviceApplication.java
@@ -0,0 +1,13 @@
+package com.diconiumwvv.storesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoresserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoresserviceApplication.class, args);
+    }
+
+}
