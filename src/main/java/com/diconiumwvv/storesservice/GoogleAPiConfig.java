5a123c5e6d2ab8db617742d951071b347b174430
schugnic <nicolai.schug@diconium.com>
2020-03-27 20:47:39 +0100

    Add google-maps-service with GoogleAPiConfig

---
+++ src/main/java/com/diconiumwvv/storesservice/GoogleAPiConfig.java
@@ -0,0 +1,17 @@
+package com.diconiumwvv.storesservice;
+
+import com.google.maps.GeoApiContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GoogleAPiConfig {
+
+    @Bean
+    public GeoApiContext geoApiContext (@Value("${google.api.key}") String apiKey) {
+        return new GeoApiContext.Builder()
+                .apiKey(apiKey)
+                .build();
+    }
+}
